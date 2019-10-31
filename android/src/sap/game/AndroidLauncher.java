86dda35071965cfb0dea9bdd4e7b4051c8e5dbc7
Mikhail <vandaniel@mail.ru>
2019-10-31 04:15:43 +0300

    Initial commit

---
+++ android/src/sap/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package sap.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import sap.game.StarGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new StarGame(), config);
+	}
+}
