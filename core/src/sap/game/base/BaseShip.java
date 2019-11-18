740ea38cbb6b21e75d58bc9ba7bd3abeeef41449
Mikhail <vandaniel@mail.ru>
2019-11-18 15:04:30 +0300

    del class BaseShip

--- core/src/sap/game/base/BaseShip.java
+++
@@ -1,9 +0,0 @@
-package sap.game.base;
-
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-
-public class BaseShip extends Sprite {
-    public BaseShip(TextureRegion region) {
-        super(region);
-    }
-}
